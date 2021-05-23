e64d74b603b340cab30e1543b517a89502c86393
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-23 16:18:51 +1000

    added innnerHTML for list items.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let myLeads = []
+let myLeads = ["www.somafm.com", "www.google.com", "www.blackcoffee.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+let ulEl = document.getElementById("ul-el")
 
+console.log(ulEl)
 
 inputBtn.addEventListener('click', function() {
-    console.log('button clicked')
+    myLeads.push(inputEl.value)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
