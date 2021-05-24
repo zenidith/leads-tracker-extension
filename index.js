a938e21360225bc3958844d20357dfcfba42cc58
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-24 23:48:33 +1000

    refactored the innerHTML of the renderLeads() to become a template string. This is much easier to read/write and I can just use string concatenation to insert my JS

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 
@@ -12,7 +13,14 @@ inputBtn.addEventListener('click', function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems  
 }
