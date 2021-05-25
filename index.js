3a341baabbdae8845cf446c01b1cd26e4bd5323c
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-25 23:44:35 +1000

    added DELETE button and styled it

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 let ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))    
-
     renderLeads()
 })
 
