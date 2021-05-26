062d95382f83247122f1ad24d29e4e8629045fa5
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-26 22:52:27 +1000

    linked Delete button to JS. Delete button now performs function on 'dblclick' which delets local storage, clears the myLeads array and then performs renderLeads again which clears the dom because the array is empty

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 let ulEl = document.getElementById("ul-el")
-
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 
 inputBtn.addEventListener('click', function() {
