4f2ff3f0baeaa231f2bedd1d6b9423934d5bf61c
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-26 23:53:14 +1000

    refactored most of the code, added TAB button, fixed Tab button to save tab in the same way as saving the Save input button

--- index.js
+++ index.js
@@ -4,36 +4,48 @@ const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 let ulEl = document.getElementById("ul-el")
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/ksheenan/"}
+]
+
+tabBtn.addEventListener("click", function() {
+    console.log(tabs[0].url)
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+     listItems += `
+     <li>
+        <a target='_blank' href=' ${leads[i]}'> 
+            ${leads[i]} 
+         </a>
+      </li>
+    `
+    }
+    ulEl.innerHTML = listItems  
+} 
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
-
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))    
-    renderLeads()
+    render(myLeads)
 })
-
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-     listItems += `<li>
-     <a target='_blank' href=' ${myLeads[i]}'> 
-     ${myLeads[i]} </a>
-      </li>
- `
-    }
-    ulEl.innerHTML = listItems  
-} 
